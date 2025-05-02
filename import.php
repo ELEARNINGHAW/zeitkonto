43906f154dd0ff007d2a188a3a00f5dc3d6a2b4f
ELEARNINGHAW <werner.welte@haw-hamburg.de>
2025-05-02 18:59:11 +0200

    V0.91

--- import.php
+++ import.php
@@ -1,13 +1,38 @@
 <?php
 
-#$facher = importFachCSV( "Zeitkontenerfassung.csv" );
+$facher = importFachCSV( "Zeitkontenerfassung.csv" );
 #deb($facher);
 
-#$entlastung = importEntlastungCSV( 'entlastung.csv' );
+importEntlastung( $facher );
+
+#importLVA($facher); ## Importiert neue LVA in die DB
+
+#importBeteiligung( $facher );
+
+/*
+foreach ($facher as $f) {
+ $T[] = $f['Gruppen'];
+ $B[] = $f['Betreuung'];
+ $K[] = $f['Anteil'];
+ $SG[] = $f[ 'Studiengruppe' ];
+}
+deb($SG);
+deb($T);
+deb($B);
+deb($K);
+*/
+/**/
+
+
+
 
 #$projekte   = importAbschlussarbeitenCSV( 'abschlussW23.csv' , 'W23' );
 
-$projekte   = importAbschlussarbeitenCSV( 'abschlussS24.csv' , 'S24' );
+#$projekte   = importAbschlussarbeitenCSV( 'abschlussS24.csv' , 'S24' );
+
+
+
+
 
 
 function checkDozent( $dozenten )
@@ -25,22 +50,151 @@ function checkDozent( $dozenten )
 $db->close();
 }
 
-function getAlleDozenten( )
+function getAlleDozenten( $param = null )
 { $db = connectDB();
   $sql = "SELECT *  FROM dozent ";
 
   foreach ($db->query($sql) as $row)
-  { $dozenten[ $row['Name']] =  $row ;
+  {
+    if     ($param == 'K') { $dozenten[ $row['Kurz'] ] =  $row ; }
+    else                   { $dozenten[ $row['Name'] ] =  $row ; }
   }
   $db->close();
   return $dozenten;
+}
+
+
+function importBeteiligung( $lvas )
+{
+  $db = connectDB();
+  foreach ( $lvas as $lva ) {
+
+## +----------------------------------------------------------------------------------------------------------------------------------------------
+  $sql3 = "SELECT COUNT(*) AS C FROM `beteiligung`";
+
+  $sql3 .= " WHERE `Jahr`     LIKE '" . ($lva['sem']['j'] + 2000) . "'";
+  $sql3 .= " AND `Semester`   LIKE '" . $lva['sem']['s'] . "'";
+  $sql3 .= " AND `DozentKurz`    LIKE '" . $lva['doz']['Kurz'] . "'";
+  $sql3 .= " AND `Fach`     LIKE '" . $lva[ 'imp' ][ 'k-base' ] . "'";
+  $sql3 .= " AND `Studiengang`     LIKE '" . $lva['Studiengruppe'] . "'";
+
+  foreach ($db->query($sql3) as $row)
+  { if ($row['C'] == 0) {
+    $sql4 = "   INSERT INTO  `beteiligung`      (`DozentKurz`, `Jahr`, `Semester`, `Fach`, `Studiengang`, `Kommentar` , `T`, `B`, `K` , `Status` ) ";
+    $sql4 .= " VALUES('" . $lva[ 'doz' ][ 'Kurz'   ] ."', '" .($lva[ 'sem' ][ 'j'      ] + 2000) ."', '" . $lva[ 'sem' ][ 's'      ] ."', '" . $lva[ 'imp' ][ 'k-base' ] ."', '" . $lva[ 'Studiengruppe' ]         ." ', '" . $lva[ 'Kommentar' ]         ." ', '" . $lva[ 'Gruppen' ]         ."', '" . $lva[ 'Betreuung' ]         ."', '" . ( $lva[ 'Anteil' ] / 100 ) . "' , '" . $lva[ 'doz' ][ 'Status'   ]      ."'); ";
+
+    deb("Import Dataset LV ! " . $sql4 );
+    $result = $db->query($sql4);
+    }
+    else
+    {  deb("Dataset already exists in lehrverpflichtung! " . $sql3);
+    }
+  }
+ }
+}
+
+
+function importEntlastung( $lvas )
+{
+  $db = connectDB();
+
+ $entlastung = importEntlastungCSV( 'entlastung.csv' );
+
+  $dozenten = getAlleDozenten( );
+
+#deb($dozenten);
+
+  foreach ( $entlastung as $ent ) {
+
+ #   deb($dozenten);
+    $ent['Grund']   =  'P' ;
+    $ent['doz']['Kurz']   = ucfirst( $dozenten[ $ent['NameDoz'] ] ['Kurz'] );
+    $ent['doz']['Status']   =  $dozenten[ $ent['NameDoz'] ] ['Status'] ;
+  $ent['LVS'] = str_replace(  ',', '.' ,$ent['LVS']);
+
+
+## +----------------------------------------------------------------------------------------------------------------------------------------------
+    $sql3 = "SELECT COUNT(*) AS C FROM `auslastung`";
+
+    $sql3 .= " WHERE `Jahr`     LIKE '" . ( $ent['sem']['j2']) . "'";
+    $sql3 .= " AND `Semester`   LIKE '" . $ent['sem']['s'] . "'";
+    $sql3 .= " AND `DozentKurz` LIKE '" . $ent['doz']['Kurz'] . "'";
+     $sql3 .= " AND `Status`     LIKE '" . $ent['doz']['Status'] . "'";
+
+    #deb($sql3,1);
+
+    foreach ($db->query($sql3) as $row)
+    { if ($row['C'] == 0) {
+
+      $sql4  ="  INSERT INTO `auslastung` (  `Jahr`, `Semester`, `DozentKurz`, `LVS`, `Grund`, `Kommentar` , `Erfassung`  , `Status`)";
+      $sql4 .=" VALUES                    (  '".$ent['sem']['j2']."', '".$ent['sem']['s']."'  , '".$ent['doz']['Kurz']."' , ".$ent['LVS'].", '".$ent['Grund']."'    , '".$ent['Thema']."', current_timestamp(), '".$ent['doz']['Status']."')";
+      deb("Import Dataset auslastung ! " . $sql4  );
+      $result = $db->query($sql4);
+    }
+    else
+    {  deb("Dataset already exists in auslastung! " . $sql3);
+    }
+    }
+  }
+}
+
+
+
+function importLVA( $lvas )
+{ $db = connectDB();
+  foreach ( $lvas as $lva )
+  {
+
+    ## +----------------------------------------------------------------------------------------------------------------------------------------------
+    $sql3  = "SELECT COUNT(*) AS C FROM `lehrverpflichtung`";
+    $sql3 .= " WHERE `Jahr`     LIKE '" . ($lva[ 'sem' ][ 'j'     ] + 2000 ) ."'";
+    $sql3 .= " AND `Semester`   LIKE '" .  $lva[ 'sem' ][ 's'     ]  ."'";
+    $sql3 .= " AND `DozKurz`    LIKE '" . $lva[ 'doz' ][ 'Kurz'   ]  ."'";
+    $sql3 .= " AND `Status`     LIKE '" . $lva[ 'doz' ][ 'Status' ]  ."'";
+
+    deb("lehrverpflichtung! " . $sql3 );
+    foreach ( $db -> query( $sql3 ) as $row )
+    { if ( $row[ 'C' ] == 0 )
+    {
+
+      $sql4 ="INSERT INTO `lehrverpflichtung` (`DozKurz` ,  `Status`  , `Jahr` , `Semester` , `Pflicht` ) ";
+      $sql4 .= "VALUES                        ('" . $lva[ 'doz' ][ 'Kurz' ] ."' , '" . $lva[ 'doz' ][ 'Status' ] ."', '" . ($lva[ 'sem' ][ 'j'      ] + 2000 ) ."' , '" . $lva[ 'sem' ][ 's'  ] ."', '".$lva['doz']['Pflicht_weg']."' )";
+
+      deb("Import Dataset LV ! ". $sql4 );
+       $result = $db -> query( $sql4 );
+     # deb( $result,1 );
+    }
+    else
+    { deb("Dataset already exists in lehrverpflichtung! " . $sql3 );
+    }
+    }
+
+
+    ## +----------------------------------------------------------------------------------------------------------------------------------------------
+    $sql1 = "SELECT COUNT(*) AS C FROM `lva` WHERE `Jahr` = ". ( $lva['sem']['j']  + 2000 ) ." AND `Semester` LIKE '".$lva['sem']['s']."' AND `FachKurz` LIKE '".$lva['imp']['k-base']."' AND `Studiengang` LIKE '" .$lva['Studiengruppe']."'";
+
+    foreach ( $db -> query( $sql1 ) as $row )
+    { if ( $row[ 'C' ] == 0 )
+      {
+        $sql ="INSERT INTO `lva`  (`Jahr`, `Semester`, `Kommentar`, `FachKurz`, `Studiengang`, `SWS` , `DepartmentKurz`)  VALUES            (".($lva['sem']['j']  + 2000 ).",        '".$lva['sem']['s']."',       '".$lva['Kommentar']."', '".$lva['imp']['k-base']."',     '".$lva['Studiengruppe'] ."' , '".$lva['LVS-Prof']."',  NULL); ";
+        deb("Import Dataset LVA! ". $sql );
+        $result = $db->query($sql);
+        deb($result,1);
+      }
+      else
+      { deb("Dataset already exists in lva! " . $sql1 );
+      }
+    }
+  #deb($sql);
+  }
+
 }
 
 function getDozentKurzName( $dozent )
 { $db = connectDB();
   $kurz = '';
   $dozent = trim(strip_tags( $dozent ) , "   ");
-  $sql = "SELECT Kurz AS K FROM dozent WHERE Name LIKE LOWER('".$dozent."')";
+  $sql = "SELECT Kurz AS K FROM dozent WHERE Name LIKE LOWER('" .$dozent. "')";
 
   foreach ($db->query($sql) as $row)
   {  $kurz =  $row['K'] ;
@@ -58,6 +212,16 @@ function getKurzNameDB( $db, $fachName )
     return  $a['k-base'];
 }
 
+function getStatusDB( $db, $fachName )
+{ $a[ 'k-base' ] ='';
+  { $sql = "SELECT `Status` FROM `fach` WHERE Name LIKE '" .$fachName. "'";
+    $res2 = $db -> query( $sql );
+    foreach ( $res2 as $r )  {  $a['k-base'] =  $r[ 'Status' ] ; }
+  }
+  return  $a['k-base'];
+}
+
+
 function getFachNameDB( $db,  $fachKurz )
 { $a[ 'n-base' ] ='';
   { $sql1 = "SELECT `Name` FROM `fach` WHERE LOWER( Kurz ) LIKE LOWER( '" .$fachKurz. "' )";
@@ -74,19 +238,19 @@ function importFachCSV( $filename )
      { $zk = null;
        if ( ++$row > 1 )
        { $zk[ trim( $h[ 0  ] )] = trim( $data[ 0  ]) ;
-         $zk[ $h[ 1  ] ] = $data[ 1  ];
-         $zk[ $h[ 2  ] ] = $data[ 2  ];
-         $zk[ $h[ 3  ] ] = $data[ 3  ];
-         $zk[ $h[ 4  ] ] = $data[ 4  ];
-         $zk[ $h[ 5  ] ] = $data[ 5  ];
-         $zk[ $h[ 6  ] ] = $data[ 6  ];
-         $zk[ $h[ 7  ] ] = $data[ 7  ];
-         $zk[ $h[ 8  ] ] = $data[ 8  ];
-         $zk[ $h[ 9  ] ] = $data[ 9  ];
-         $zk[ $h[ 10 ] ] = $data[ 10 ];
-         $zk[ $h[ 11 ] ] = $data[ 11 ];
-         $zk[ $h[ 12 ] ] = $data[ 12 ];
-         $zk[ $h[ 13 ] ] = $data[ 13 ];
+         $zk[ $h[ 1  ] ] = trim( $data[ 1  ]) ;
+         $zk[ $h[ 2  ] ] = trim( $data[ 2  ]) ;
+         $zk[ $h[ 3  ] ] = trim( $data[ 3  ]) ;
+         $zk[ $h[ 4  ] ] = trim( $data[ 4  ]) ;
+         $zk[ $h[ 5  ] ] = trim( $data[ 5  ]) ;
+         $zk[ $h[ 6  ] ] = trim( $data[ 6  ]) ;
+         $zk[ $h[ 7  ] ] = trim( $data[ 7  ]) ;
+         $zk[ $h[ 8  ] ] = trim( $data[ 8  ]) ;
+         $zk[ $h[ 9  ] ] = trim( $data[ 9  ]) ;
+         $zk[ $h[ 10 ] ] = trim( $data[ 10  ]) ;
+         $zk[ $h[ 11 ] ] = trim( $data[ 11  ]) ;
+         $zk[ $h[ 12 ] ] = trim( $data[ 12  ]) ;
+         $zk[ $h[ 13 ] ] = trim( $data[ 13  ]) ;
 
          $zeitkonten[] = $zk;
 
@@ -133,15 +297,12 @@ function importFachCSV( $filename )
     $f[] = $zk;
   }
 
-  # deb( $g1 );
-  # deb( $g2 );
+  #deb( $g1 );
+   #deb( $g2 ,1);
   # deb($f);
     return $f;
     #$db->close();
 }
-
-
-
 
 function importEntlastungCSV( $filename )
 { $row = 0;
@@ -155,26 +316,17 @@ function importEntlastungCSV( $filename )
       $zk[ trim( $h[ 3  ] ) ] = $data[ 3  ];
       $zk[ trim( $h[ 4  ] ) ] = $data[ 4  ];
       $zk[ trim( $h[ 5  ] ) ] = $data[ 5  ];
- #     $zk[ $h[ 6  ] ] = $data[ 6  ];
- #     $zk[ $h[ 7  ] ] = $data[ 7  ];
- #     $zk[ $h[ 8  ] ] = $data[ 8  ];
- #     $zk[ $h[ 9  ] ] = $data[ 9  ];
- #     $zk[ $h[ 10 ] ] = $data[ 10 ];
- #     $zk[ $h[ 11 ] ] = $data[ 11 ];
- #     $zk[ $h[ 12 ] ] = $data[ 12 ];
- #     $zk[ $h[ 13 ] ] = $data[ 13 ];
+      $zk[ 'sem' ] = redefSemester(  $zk[ trim( $h[ 1  ] ) ] );
 
       $zeitkonten[] = $zk;
-
- #     $dozent[ $row ] = $data[ 0 ];
- #     $fach[   $row ] = $data[ 3 ];
     }
     else{  $h = $data; }
   }
     fclose( $handle );
   }
-  deb($zeitkonten,1);
 
+  #deb($zeitkonten);
+return $zeitkonten;
 }
 
 
@@ -192,17 +344,8 @@ function importAbschlussarbeitenCSV( $filename,$sem )
       $zk[ trim( $h[ 5  ] ) ] = $data[ 5  ];
       $zk[ $h[ 6 ] ] = $data[ 6 ];
       $zk[ $h[ 7 ] ] = $data[ 7 ];
-      #     $zk[ $h[ 8  ] ] = $data[ 8  ];
-      #     $zk[ $h[ 9  ] ] = $data[ 9  ];
-      #     $zk[ $h[ 10 ] ] = $data[ 10 ];
-      #     $zk[ $h[ 11 ] ] = $data[ 11 ];
-      #     $zk[ $h[ 12 ] ] = $data[ 12 ];
-      #     $zk[ $h[ 13 ] ] = $data[ 13 ];
 
       $zeitkonten[] = $zk;
-
-      #     $dozent[ $row ] = $data[ 0 ];
-      #     $fach[   $row ] = $data[ 3 ];
 
       if ( $data[ 2 ] != '' )
       { $curDoz = $data[ 2  ];
@@ -211,14 +354,9 @@ function importAbschlussarbeitenCSV( $filename,$sem )
         $t[ 'Sem' ] = $sem;
       }
 
-
-
       $t[ trim( $h[ 7  ] ) ] = $data[ 7  ];
 
       $d[   $curDoz  ] =  $t;
-
-
-
 
     }
     else{  $h = $data; }
@@ -237,7 +375,10 @@ function importAbschlussarbeitenCSV( $filename,$sem )
 function redefSemester( $sem )
 { if ( stristr($sem, 'WiSe' ) )   { $s[ 's' ] = 'W';  }
   if ( stristr($sem, 'SoSe' ) )   { $s[ 's' ] = 'S';  }
+
   $s[ 'j' ] = substr( $sem, 5, 2 );
+  $s[ 'j2' ] =  $s[ 'j' ] + 2000 ;
+
   return $s;
 }
 
